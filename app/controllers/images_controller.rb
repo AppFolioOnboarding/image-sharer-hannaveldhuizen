24f20b55db3c77b943522cd84401216e623069e0
Hanna Veldhuizen <hannaveldhuizen@email.arizona.edu>
2019-07-02 15:29:55 -0700

    hv/yc - Add show endpoint to image controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,14 @@ class ImagesController < ApplicationController
   def create
     @image = Image.new(params.require(:image).permit(:url))
 
-    @image.save
+    if @image.save
+      redirect_to @image
+    else
+      render :new
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
   end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -16,11 +16,21 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Image.count', 1) do
       post images_url, params: { image: { url: 'https://hello.com' } }
     end
+
+    assert_redirected_to image_url(Image.last)
   end
 
   test 'should not create an image with an invalid url' do
     assert_no_difference('Image.count') do
       post images_url, params: { image: { url: 'hello' } }
     end
+  end
+
+  test 'should show image' do
+    @image.update(url: 'https://hello.com')
+    get image_url(@image)
+    assert_response :success
+    assert_select 'img', count: 1
+    assert_select 'h1', "ID: #{@image.id}"
   end
 end
