399c66d7c9cf0eaad810afba8cf8b748ced8ee1b
Hanna Veldhuizen <hannaveldhuizen@email.arizona.edu>
2019-07-05 12:21:13 -0700

    hv - Seed database with 20 random images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.create(url: 'https://cdn.spacetelescope.org/archives/images/large/heic0910e.jpg')
+
+Image.create(url: 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+
+Image.create(url: 'https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499')
+
+Image.create(url: 'https://images.unsplash.com/photo-1534278931827-8a259344abe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
+
+Image.create(url: 'https://secure.i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg')
+
+Image.create(url: 'https://www.istockphoto.com/resources/images/HomePage/FourPack/2018_05/Illustrations_499178960.jpg')
+
+Image.create(url: 'https://www.gettyimages.co.uk/gi-resources/images/frontdoor/creative/PanoramicImagesRM/FD_image.jpg')
+
+Image.create(url: 'https://cdn.cnn.com/cnnnext/dam/assets/190121090951-04-blood-moon-global-01212019-exlarge-169.jpg')
+
+Image.create(url: 'https://s-ec.bstatic.com/images/hotel/max1024x768/460/46072868.jpg')
+
+Image.create(url: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg')
+
+Image.create(url: 'https://www.nationalgeographic.com/content/dam/travel/photos/000/861/86178.ngsversion.1471323001675.adapt.1900.1.jpg')
+
+Image.create(url: 'https://files.brightside.me/files/news/part_41/412410/17693060-288z1jHYyvb3py6wQAPV5SnHjg2_IHhPZDYzievnKf8-1512150794-650-8d09c52ebf-1512844656.jpg')
+
+Image.create(url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrEaq15GA-mDcb61vRZEPklnOVAfTfsV3yvaOeJ2HWHppz0h9I')
+
+Image.create(url: 'https://www.yourtango.com/sites/default/files/styles/header_slider/public/image_blog/michael%20scott.jpg?itok=ihtKygg5')
+
+Image.create(url: 'https://imgix.bustle.com/rehost/2016/9/13/ffa8cec2-85d9-40ad-b386-906b75bf5610.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70&dpr=2')
+
+Image.create(url: 'http://2.media.collegehumor.cvcdn.com/83/10/2ccb957dd9f09d3f8dcfba7051ea84be.jpg')
+
+Image.create(url: 'https://i.pinimg.com/originals/3e/7a/36/3e7a3606164b0f6c5af5d59aaf93cc13.jpg')
+
+Image.create(url: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+
+Image.create(url: 'https://i.redd.it/fxlo95dg66vz.jpg')
+
+Image.create(url: 'https://everydaypower.com/wp-content/uploads/2017/03/76-Inspirational-picture-quotes-and-motivational-images.jpg')
