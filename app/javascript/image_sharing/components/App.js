63a545a9232a70a77c5ef446ab50b2bc22e15f2d
Hanna Veldhuizen <hannaveldhuizen@email.arizona.edu>
2019-07-09 14:19:45 -0700

    hv/cp - Add footer

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import FeedbackForm from './FeedbackForm';
+import Footer from './Footer';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header title="Tell us what you think" />
       <div className="d-flex align-items-center flex-column">
         <FeedbackForm />
+        <Footer text="Copyright: Appfolio Inc. Onboarding" />
       </div>
     </div>
   );
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -5,11 +5,13 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
 import FeedbackForm from '../../components/FeedbackForm';
+import Footer from '../../components/Footer';
 
 describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(FeedbackForm)).to.have.length(1);
     expect(wrapper.contains('Tell us what you think'));
+    expect(wrapper.find(Footer)).to.have.length(1);
   });
 });
