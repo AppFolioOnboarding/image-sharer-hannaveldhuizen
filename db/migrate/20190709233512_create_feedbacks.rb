28cc4e521220735f3823e323961a7473b402c753
Hanna Veldhuizen <hannaveldhuizen@email.arizona.edu>
2019-07-10 13:47:03 -0700

    hv - Feedback db migration

---
+++ db/migrate/20190709233512_create_feedbacks.rb
@@ -0,0 +1,10 @@
+class CreateFeedbacks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feedbacks do |t|
+      t.string :name
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end
