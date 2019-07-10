812558a02e88441f4777c0f6384be6e5103c91eb
Hanna Veldhuizen <hannaveldhuizen@email.arizona.edu>
2019-07-10 14:20:02 -0700

    hv - add create action for feedback

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,13 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      @feedback = Feedback.new(params.require(:feedback).permit(:name, :comments))
+
+      if @feedback.save
+        render status: :ok, json: { message: 'success' }
+      else
+        render status: :unprocessable_entity, json: { message: 'error' }
+      end
     end
   end
 end
